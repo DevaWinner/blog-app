efe184fe5d7ad2fa9c9e63fd7a424ae50cd684c7
DevaWinner <winneraaniekan@gmail.com>
2023-09-30 23:13:18 +0100

    Add validation to post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,13 +11,15 @@ class Post < ApplicationRecord
   after_create :update_posts_counter
   after_destroy :update_posts_counter
 
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-
-  private
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
+  end
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
