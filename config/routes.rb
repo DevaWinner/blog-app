164c2c1d01b065292af58eca5f35cd91cb04c7fa
DevaWinner <winneraaniekan@gmail.com>
2023-11-11 21:27:41 +0100

    Refactor controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,9 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments ,only:[:index,:new,:create, :destroy]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments ,only:[:index,:new,:create]
       resources :likes ,only:[:create]
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      resources :users do
-        resources :posts, only: [:index] do
-         resources :comments, only: [:index, :create]
-        end
-      end
     end
   end
 end
