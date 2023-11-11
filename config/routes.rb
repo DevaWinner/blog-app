90e1fabafeab41fe4a2ce18807ece15f58d3c989
DevaWinner <winneraaniekan@gmail.com>
2023-11-11 21:23:19 +0100

    Add devise authentication and API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth',
+                     path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    root to: 'devise/sessions#new'
+  end
+  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:index, :new, :create]
-      resources :likes, only: [:create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments ,only:[:index,:new,:create, :destroy]
+      resources :likes ,only:[:create]
     end
   end
-  root 'users#index'
+  namespace :api do
+    namespace :v1 do
+      resources :users do
+        resources :posts, only: [:index] do
+         resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
